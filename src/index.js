81face7aa9fe6d7a42e6b1b8a3489bba571db4e0
Tyrone <tschoeman7@gmail.com>
2024-11-21 19:32:05 +0200

    added todo list checkbox, if checkbox clicked strikethrough todo list item

--- src/index.js
+++ src/index.js
@@ -1,43 +1,45 @@
-
 // index.js
-import {addToDo} from "./todo-input.js";
+import { addToDo } from "./todo-input.js";
 import './styles.css'
 
 let ul = document.getElementById("todo-parent");
+let incrementedId = 2;
 
-let incrementedId = 2
-let displayUserInput = function(id){
+// Function to display user input and add a checkbox
+let displayUserInput = function(id) {
+    let createCheckBox = document.createElement("input");
+    createCheckBox.type = "checkbox";
+    createCheckBox.setAttribute("class", "box");
+    
+    let userInput = prompt("Enter a todo!");
+    let p = document.getElementById(id);
+    p.textContent = userInput;
 
-    let userInput = prompt("enter a todo!");
-    let li = document.getElementById(id);
-    li.textContent = userInput;
-    // ul.appendChild(li);
-
-
-}
-
-
-let createList = function(){
-
-    let newListItem = document.createElement("li");
-    newListItem.textContent = '+ add to do!';
-    ul.appendChild(newListItem);
-    newListItem.setAttribute("id",incrementedId++);
-    console.log(incrementedId);
-}
-
-
-let displayTodo = function(){
-
+    // Append the checkbox to the todo item
+    p.appendChild(createCheckBox);
 };
 
+// Function to create a new to-do list item
+let createList = function() {
+    let newPara = document.createElement("p");
+    newPara.textContent = '+ Add Todo!';
+    ul.appendChild(newPara);
+    newPara.setAttribute("id", incrementedId++);
+    console.log(incrementedId);
+};
 
+// Event delegation for dynamically added checkboxes
+ul.addEventListener('change', function(e) {
+    if (e.target && e.target.classList.contains('box')) {
+        // Toggle the 'checked' class when checkbox state changes
+        e.target.parentNode.classList.toggle('checked');
+    }
+});
 
-
-
+// Add event listener to the 'addToDo' button
 addToDo.addEventListener('click', (e) => {
-    let ElementId = e.target.id
+    let ElementId = e.target.id;
     displayUserInput(ElementId);
-    createList();
+    createList()
 
-    })
+});
