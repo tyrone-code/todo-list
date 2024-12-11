261c458f0685e3e10d398e5a459dfeb22e8d8c1a
Tyrone <tschoeman7@gmail.com>
2024-12-11 18:36:20 +0200

    added textbox input and enter event to confirm and disply todo with checkbox

--- src/index.js
+++ src/index.js
@@ -1,45 +1,63 @@
+
+
 // index.js
 import { addToDo } from "./todo-input.js";
 import './styles.css'
 
 let ul = document.getElementById("todo-parent");
+// let button = document.getElementById("button");
+
 let incrementedId = 2;
 
 
-const variableDump = {
+const globalVariables = {
      userPrompt: function(){
         return prompt("enter a todo!")
      },
      userResponse: false,
 }
-// Function to display user input and add a checkbox
-const displayUserInput = function(id) {
-    let storedTxt = variableDump.userPrompt();
-     
-    if(storedTxt !== null){
-        variableDump.userResponse = true;
-        let createCheckBox = document.createElement("input");
-        createCheckBox.type = "checkbox";
-        createCheckBox.setAttribute("class", "box");
-        let p = document.getElementById(id);
-        p.textContent = storedTxt;
-    
-        // Append the checkbox to the todo item
-        p.appendChild(createCheckBox);
-    }else {
-        return
+
+    const makeUserInputTextBox = function(){
+        let input = document.createElement("input")
+        input.setAttribute('type', 'text');
+        input.setAttribute('id', 'user-input');
+        return input;
     }
+ 
+
+    
+
+const displayUserInput = () => {
+  const textInput = document.getElementById("user-input");
+ let paragraph =  document.createElement("p")
+  paragraph.textContent  = textInput.value 
+  paragraph.setAttribute("id",incrementedId++)
+  textInput.replaceWith(paragraph)
+  if(paragraph !== null){
+    let createCheckBox = document.createElement("input");
+    createCheckBox.type = "checkbox";
+    createCheckBox.setAttribute("class", "box");
+    paragraph.appendChild(createCheckBox)
+  }
+ 
+
+}
+
+
+// Function to display user input and add a checkbox
+const getInputTextBox = function(id) {
+        let  addToDoTextPara = document.getElementById(id);
+     addToDoTextPara.replaceWith(makeUserInputTextBox());
+        let createdInput = document.getElementById('user-input')
+        createdInput.focus();
 };
-// console.log(displayUserInput())
 // Function to create a new to-do list item
 const createList = function() {
-    if(variableDump.userResponse){
         let newPara = document.createElement("p");
         newPara.textContent = '+ Add Todo!';
         ul.appendChild(newPara);
         newPara.setAttribute("id", incrementedId++);
-    }
-
+  
 };
 
 // Add event listener to the 'addToDo' button
@@ -50,16 +68,88 @@ ul.addEventListener('click', (e) => {
         e.target.parentNode.classList.toggle('checked');
     }else {
         let ElementId = e.target.id;
-        displayUserInput(ElementId);
+        getInputTextBox(ElementId);
         createList()
     
     }
 
 });
 
+document.addEventListener('keydown', keyPressed);
 
+function keyPressed(e) {
+  if(e.code === "Enter") {
+    displayUserInput()
+}
+}
 
 // Event delegation for dynamically added checkboxes
-ul.addEventListener('change', function(e) {
-   
-});
+// button.addEventListener('click', function(e) {
+//    alert()
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
